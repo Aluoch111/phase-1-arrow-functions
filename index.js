b2ab4b69bf793a405b4384721517f999a49c9c45
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-08-20 12:45:56 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = (num1,num2) => {
+    num1 = 2000 ;
+    num2 = 100 ;
+    return num1 / num2 ;
+}
+const square = (n) => {
+    return n * n ;
+}
+const add = (n,n1) => {
+    return n + n1 ;
+}
